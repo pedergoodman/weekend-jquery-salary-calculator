c2d5bad0d17e0305248423ca37a4246bb65ed2c2
Peder Goodman <peder.goodman@gmail.com>
2023-06-09 22:37:44 -0500

    condensed background change to function

--- salary-calc.js
+++ salary-calc.js
@@ -1,52 +1,47 @@
-$(document).ready(onReady)
+$(document).ready(onReady);
 
 let totalAnnualSalary = 0;
 let totalMonthlySalary = 0;
 
-
 function onReady() {
-    console.log('hey js & jQ');
+  console.log("hey js & jQ");
 
-    // Listener for submit button
-    $('#submit-button').on('click', addEmployee)
+  // Listener for submit button
+  $("#submit-button").on("click", addEmployee);
 
-    // delegated listener for delete button
-    $('#table-body').on('click', '.delete', deleteEmployee)
+  // delegated listener for delete button
+  $("#table-body").on("click", ".delete", deleteEmployee);
 
-    // Listen for total monthly cost to go above 20K?
-    
 }
 
-
-
 // Event handler for Submit button
-    // What it actually does
-        // take each form input 
-        // store in variable
-        // write and append to table element
-        // send off numbers to math function
-        // clear form
+// What it actually does
+// take each form input
+// store in variable
+// write and append to table element
+// send off numbers to math function
+// clear form
 function addEmployee(event) {
-    // event prevent
-    event.preventDefault();
-    
-    // console.log("inside addEmployee");
+  // event prevent
+  event.preventDefault();
 
-    // grab form data
-    let firstNameValue = $('#input-first-name').val()
-    let lastNameValue = $('#input-last-name').val()
-    let employeeIdValue = +$('#input-employee-id').val()
-    let jobTitleValue = $('#input-job-title').val()
-    let annualSalaryValue = +$('#input-salary').val()
-    
-    // Grab Value Tests
-    console.log('firstNameValue is:', firstNameValue);
-    console.log('lastNameValue is:', lastNameValue);
-    console.log('employeeIdValue is:', employeeIdValue);
-    console.log('jobTitleValue is:', jobTitleValue);
-    console.log('annualSalaryValue is:', annualSalaryValue);
+  // console.log("inside addEmployee");
 
-    $('#table-body').append(`
+  // grab form data
+  let firstNameValue = $("#input-first-name").val();
+  let lastNameValue = $("#input-last-name").val();
+  let employeeIdValue = +$("#input-employee-id").val();
+  let jobTitleValue = $("#input-job-title").val();
+  let annualSalaryValue = +$("#input-salary").val();
+
+  // Grab Value Tests
+  console.log("firstNameValue is:", firstNameValue);
+  console.log("lastNameValue is:", lastNameValue);
+  console.log("employeeIdValue is:", employeeIdValue);
+  console.log("jobTitleValue is:", jobTitleValue);
+  console.log("annualSalaryValue is:", annualSalaryValue);
+
+  $("#table-body").append(`
     <tr>
     <td>${firstNameValue}</td>
     <td>${lastNameValue}</td>
@@ -55,85 +50,79 @@ function addEmployee(event) {
     <td>$${annualSalaryValue.toLocaleString("en-US")}</td>
     <td><button class="delete">Delete</button></td>
     </tr>
-    `)
-    
-    // store data 
-    
+    `);
 
-    addToMonthly(annualSalaryValue);
-    
-    //clear form
-    $('#input-first-name').val('')
-    $('#input-last-name').val('')
-    $('#input-employee-id').val('')
-    $('#input-job-title').val('')
-    $('#input-salary').val('')
+  // store data
 
-}
+  addToMonthly(annualSalaryValue);
+
+  //clear form
+  $("#input-first-name").val("");
+  $("#input-last-name").val("");
+  $("#input-employee-id").val("");
+  $("#input-job-title").val("");
+  $("#input-salary").val("");
+} // end addEmployee
 
 function deleteEmployee() {
-    console.log('inside deleteEmployee');
+  console.log("inside deleteEmployee");
 
-    let testVal = $(this).closest('tr').get()
-    console.log(testVal);
+  let testVal = $(this).closest("tr").get();
+  console.log(testVal);
 
+  // removeFromMonthly(.toLocaleNumber())
 
-    // removeFromMonthly(.toLocaleNumber())
+  $(this).closest("tr").remove();
 
-    $(this).closest('tr').remove()
+  // I want to grab the employee id?
+  // save for later
+  // let firstNameValue = $(this).closest(tr).
+  // let value = $(this).parent().parent()
+  // console.log(value);
+} // end deleteEmployee
 
-    
-    
-    // I want to grab the employee id? 
-    // save for later
-    // let firstNameValue = $(this).closest(tr).
-        // let value = $(this).parent().parent()
-        // console.log(value);
-}
-
-
-// CALC for Monthly cost 
+// CALC for Monthly cost
 function addToMonthly(salaryInput) {
-    // take salary input
-    console.log('in addToMonthly, value passed is', salaryInput);
-    // store in some variables
-    totalAnnualSalary += salaryInput
-    // calculate total monthly
-    totalMonthlySalary = totalAnnualSalary / 12
-    // Tests math
-    // console.log('totalAnnualSalary', totalAnnualSalary);
-    // console.log('totalMonthlySalary', totalMonthlySalary);
-    
-    // append to DOM
-    $('#monthly-cost').text(totalMonthlySalary.toLocaleString("en-US"))
+  // take salary input
+  console.log("in addToMonthly, value passed is", salaryInput);
+  // store in some variables
+  totalAnnualSalary += salaryInput;
+  // calculate total monthly
+  totalMonthlySalary = totalAnnualSalary / 12;
+  // Tests math
+  // console.log('totalAnnualSalary', totalAnnualSalary);
+  // console.log('totalMonthlySalary', totalMonthlySalary);
 
-    if (totalMonthlySalary > 20000) {
-        $('#monthly-cost').parent().css("background", "red")
-    } else {
-        $('#monthly-cost').parent().css("background", "default")
-    }
-}
+  // append to DOM
+  $("#monthly-cost").text(totalMonthlySalary.toLocaleString("en-US"));
 
-// needs connection --> removedSalary = Annual Salary data
+  // checks if totalMonthlySalary > 20K
+  totalMonthlyBackground();
+} // end addToMonthly
+
+// CALC needs connection --> removedSalary = Annual Salary data
 function removeFromMonthly(removedSalary) {
-    console.log('in removeFromMonthly, value passed is:', removedSalary);
-    // update variables
-    totalAnnualSalary -= removedSalary
-    // calculate new total monthly
-    totalMonthlySalary = totalAnnualSalary / 12
-    // Tests math
-    console.log('totalAnnualSalary', totalAnnualSalary);
-    console.log('totalMonthlySalary', totalMonthlySalary);
-    $('#monthly-cost').text(totalMonthlySalary.toLocaleString("en-US"))
+  console.log("in removeFromMonthly, value passed is:", removedSalary);
+  // update variables
+  totalAnnualSalary -= removedSalary;
+  // calculate new total monthly
+  totalMonthlySalary = totalAnnualSalary / 12;
+  // Tests math
+  console.log("totalAnnualSalary", totalAnnualSalary);
+  console.log("totalMonthlySalary", totalMonthlySalary);
+  $("#monthly-cost").text(totalMonthlySalary.toLocaleString("en-US"));
 
-    if (totalMonthlySalary > 20000) {
-        $('#monthly-cost').parent().css("background", "red")
-    } else {
-        $('#monthly-cost').parent().css("background", "default")
-    }
+  // checks if totalMonthlySalary > 20K
+  totalMonthlyBackground();
+} // end removeFromMonthly
 
-}
-
+function totalMonthlyBackground() {
+  if (totalMonthlySalary > 20000) {
+    $("#monthly-cost").parent().css("background", "red");
+  } else {
+    $("#monthly-cost").parent().css("background", "default");
+  }
+} // end totalMonthlyBackground
 
 // while (totalMonthlySalary < 20000) {
 //     $('#monthly-cost').css("background-color", "red")
